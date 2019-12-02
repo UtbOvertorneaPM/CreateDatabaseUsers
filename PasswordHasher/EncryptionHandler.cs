a2942da1ba7d3b33c45dc128b8f4883502b97b92
UtbOvertorneaPM <peter.mortberg@utb.overtornea.se>
2019-12-02 07:25:49 +0100

    updated the number of permutations in the hashing, updated the read me

--- PasswordHasher/EncryptionHandler.cs
+++ PasswordHasher/EncryptionHandler.cs
@@ -10,7 +10,7 @@ namespace PasswordHasher {
             var saltSize = 32;
             var keySize = 512;
 
-            using (var algorithm = new Rfc2898DeriveBytes(password, saltSize, 1000, HashAlgorithmName.SHA512)) {
+            using (var algorithm = new Rfc2898DeriveBytes(password, saltSize, 12000, HashAlgorithmName.SHA512)) {
 
                 var key = Convert.ToBase64String(algorithm.GetBytes(keySize));
                 var salt = Convert.ToBase64String(algorithm.Salt);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PasswordHasher
-Command Line Interface tool for creating passwords and inserting users for a local MySql database
+Command Line Interface tool for creating passwords and inserting users to a local MySql database
+
+To use follow the on-screen directions for either creating a salted password hash or to create a user with password in a local MySql database.
